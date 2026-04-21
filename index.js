95006b6b97c2f685a086174f061ff9ed6901625a
Ivan Gusarov <gusarivan52@gmail.com>
2026-04-21 19:32:46 +0500

    task 2

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-
 const form = document.querySelector('form');
 const addBeverageBtn = document.querySelector('.add-button');
 const addBeverageBtnWrapper = addBeverageBtn.parentElement;
+
+function setupRemoveButton(beverageElement) {
+    const crossBtn = beverageElement.querySelector('.cross');
+    crossBtn.addEventListener('click', () => {
+        const allBeverages = document.querySelectorAll('.beverage');
+        if (allBeverages.length > 1) {
+            beverageElement.remove();
+        } else {
+            alert("Должен остаться хотя бы один напиток!");
+        }
+    });
+}
+
+setupRemoveButton(document.querySelector('.beverage'));
 
 addBeverageBtn.addEventListener('click', () => {
     const currentBeverages = document.querySelectorAll('.beverage');
@@ -9,5 +22,6 @@ addBeverageBtn.addEventListener('click', () => {
     const newNumber = currentBeverages.length + 1;
     const newBeverageCount = newBeverage.querySelector('.beverage-count');
     newBeverageCount.textContent = `Напиток №${newNumber}`;
-    form.insertBefore(newBeverage, addBeverageBtnWrapper)
+    form.insertBefore(newBeverage, addBeverageBtnWrapper);
+    setupRemoveButton(newBeverage);
 });
